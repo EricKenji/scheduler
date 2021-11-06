988ba5740ee7de45e5db06426d4f56b7d4d5ea9a
Eric Furukawa <ericfurukawa@erics-mbp.lan>
2021-11-06 15:52:04 -0700

    adding color coding function

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,36 @@
-var d = new Date();
-document.getElementById("currentDay").innerHTML = d;
+// jquery ready function
+$( document ).ready(function() {
+
+    // current date using moment//
+    $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+    // time color status // 
+    var timeColor = function () {
+        var presentHour = moment().hour()
+
+        $('.time-block').each(function() {
+                var timeBlock = parseInt($(this).attr("id"));
+                console.log(presentHour);
+                console.log(timeBlock);
+
+                if (timeBlock < presentHour) {
+                    $(this).addClass('past');
+                    $(this).removeClass('present');
+                    $(this).removeClass('future');
+                }
+                else if (timeBlock === presentHour) {
+                    $(this).removeClass('past');
+                    $(this).addClass('present');
+                    $(this).removeClass('future');
+                } else {
+                    $(this).removeClass('past');
+                    $(this).removeClass('present');
+                    $(this).addClass('future');
+                }
+        });
+    }
+
+    timeColor();
+
+
+});
