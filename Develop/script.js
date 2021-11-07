03112dc2b5a6878ba744ea35ab7689c4113d1c81
Eric Furukawa <ericfurukawa@Erics-MacBook-Pro.local>
2021-11-07 08:39:11 -0800

    added getitem to load localstorage

--- Develop/script.js
+++ Develop/script.js
@@ -30,9 +30,22 @@ $( document ).ready(function() {
 
     // local storage function //
     $('.saveBtn').on('click', function() {
-        var taskText = $(this).siblings(".description").val();
         var timeValue = $(this).parent().attr("id");
+        var taskText = $(this).siblings(".description").val();
         localStorage.setItem(timeValue, taskText);    })
+
+    // get items from local storage and load them into time block when page refreshes
+    $("#8 .description").val(localStorage.getItem("8"));
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+    $("#18 .description").val(localStorage.getItem("18"));
 
     // call time color function
     timeColor();
